1d91636945abc8fe9ab9bcff04543a7add44797a
Thales Pinheiro <thales@bravocognos.com>
2019-11-07 00:31:45 -0800

    Improves cache, scoping it per application
    Signed-off-by: Thales Pinheiro <thales@bravocognos.com>

--- cache.js
+++ cache.js
@@ -20,34 +20,40 @@ const secFilename = process.env.VAULT_CACHE_FILENAME || ".sec.json";
 /**
  * Tries to load the cache from disk. The cache is decoded with Base64.
  *
+ * @param {String} appName
+ *
  * @returns Cached configurations
  */
-const load = () => {
+const load = (appName) => {
 	let configurations;
 
 	try {
 		const pathToConfiguration = path.resolve(secFilename);
 		configurations = require(pathToConfiguration);
-		configurations = Buffer.from(configurations.sec, "base64").toString("ascii");
+		configurations = Buffer.from(configurations[appName].sec, "base64").toString("ascii");
 		configurations = JSON.parse(configurations);
 	} catch (error) {
-		throw new Error("Failed to load cached configurations");
+		throw new Error(`Failed to load cached configurations! Error: ${error}`);
 	}
+
 	return configurations;
 };
 
 /**
  * Tries to cache the configurations. The configurations will be encoded with Base64.
  *
+ * @param {String} appName
  * @param {Object} configurations
  */
-const create = (configurations) => {
+const create = (appName, configurations) => {
 	const configurationsStringified = JSON.stringify(configurations);
 	const configurationsBase64 = Buffer.from(configurationsStringified).toString("base64");
 
 	try {
 		FS.writeFileSync(path.resolve(secFilename), JSON.stringify({
-			"sec": configurationsBase64
+			[appName]: {
+				"sec": configurationsBase64
+			}
 		}));
 	} catch (error) {
 		throw new Error(`Failed to create cache! Error: ${error.message}`);
